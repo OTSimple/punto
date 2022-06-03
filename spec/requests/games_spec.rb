36d2c07aade79d2582b9a10ddc402e616e5e7167
MacBook <gregoire.castellazzi@laposte.net>
2022-06-03 09:44:34 +0200

    Added devise guest user

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Games", type: :request do
   describe "POST /create" do
     it "returns http success" do
-      post "/games", params: { game: { user_pseudo: "Test" }}
+      post "/games"
       expect(response).to have_http_status(:success)
     end
   end
